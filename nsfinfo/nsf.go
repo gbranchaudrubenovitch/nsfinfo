b79e1b93cf36d927dc3cd5c7e552b281a5c28c82
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2014-09-15 23:52:34 -0400

    completed the output of the header.

--- nsfinfo/nsf.go
+++ nsfinfo/nsf.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"strings"
 )
@@ -25,33 +26,87 @@ const (
 	futureChip2 // must never be set
 )
 
+// nesWord is a little endian uint16
+type nesWord [2]byte
+
+func (w nesWord) toUInt16() uint16 {
+	return binary.LittleEndian.Uint16(w[:])
+}
+
 // nsfHeader exposes the nsf header fields (more info at http://wiki.nesdev.com/w/index.php/NSF)
 type nsfHeader struct {
 	Prelude          [5]byte
 	Version          int8
 	TotalSongs       int8
 	StartingSong     int8
-	LoadAddress      int16
-	InitAddress      int16
-	PlayAddress      int16
+	LoadAddress      nesWord
+	InitAddress      nesWord
+	PlayAddress      nesWord
 	SongName         [32]byte
 	Artist           [32]byte
 	CopyrightHolder  [32]byte
-	NTSCPlaySpeed    int16
-	BankswitchInit   [8]byte
-	PalPlaySpeed     int16
+	NTSCPlaySpeed    nesWord
+	BankswitchInit   int64 // we do not care about the specific banks assignment
+	PalPlaySpeed     nesWord
 	RegionFlags      regionFlag
 	ExtraChipFlags   extraChipFlag
 	ExpansionPadding int32 // always 0
 }
 
 func (h nsfHeader) String() string {
-	return fmt.Sprintf("%-17s: %s\n%-17s: %s\n%-17s: %s\n%-17s: %d\n%-17s: %#x\n",
+	return fmt.Sprintf("%-23s: %s\n%-23s: %s\n%-23s: %s\n%-23s: %d\n%-23s: %d\n%-23s: %s\n%-23s: %d\n----------------\n%-23s: %d\n%-23s: %v\n%-23s: %s\n%-23s: %#x\n%-23s: %#x\n%-23s: %#x",
 		"name", trimNull(h.SongName[:]),
 		"artist", trimNull(h.Artist[:]),
 		"copyright holder", trimNull(h.CopyrightHolder[:]),
 		"total # of songs", h.TotalSongs,
-		"load address", h.LoadAddress)
+		"first song", h.StartingSong,
+		"region", h.region(),
+		"play speed (μs)", h.playSpeed().toUInt16(),
+		"nsf version", h.Version,
+		"uses bankswitching", h.BankswitchInit != 0,
+		"expansion chips in use", h.extraChips(),
+		"load address", h.LoadAddress.toUInt16(),
+		"init address", h.InitAddress.toUInt16(),
+		"play address", h.PlayAddress.toUInt16(),
+	)
+}
+
+func (h nsfHeader) playSpeed() nesWord {
+	if h.RegionFlags&pal != 0 {
+		return h.PalPlaySpeed
+	}
+	return h.NTSCPlaySpeed
+}
+
+func (h nsfHeader) region() string {
+	if h.RegionFlags&dual != 0 {
+		return "dual PAL/NTSC"
+	} else if h.RegionFlags&pal != 0 {
+		return "PAL"
+	}
+	return "NTSC"
+}
+
+func (h nsfHeader) extraChips() string {
+	// todo: replace those ugly if/else with a const [extraChipFlag, string]map
+	var chipsInUse []string
+	if h.ExtraChipFlags&vrc6 != 0 {
+		chipsInUse = append(chipsInUse, "VRC6")
+	} else if h.ExtraChipFlags&vrc7 != 0 {
+		chipsInUse = append(chipsInUse, "VRC7")
+	} else if h.ExtraChipFlags&fds != 0 {
+		chipsInUse = append(chipsInUse, "Famicom Disk System")
+	} else if h.ExtraChipFlags&mmc5 != 0 {
+		chipsInUse = append(chipsInUse, "mmc5")
+	} else if h.ExtraChipFlags&namco163 != 0 {
+		chipsInUse = append(chipsInUse, "Namco 163")
+	} else if h.ExtraChipFlags&sunsoft5b != 0 {
+		chipsInUse = append(chipsInUse, "Sunsoft 5B")
+	}
+	if len(chipsInUse) == 0 {
+		chipsInUse = append(chipsInUse, "none")
+	}
+	return fmt.Sprintf("%v", chipsInUse) // do a better comma-delimited formatting here
 }
 
 func trimNull(s []byte) string {
