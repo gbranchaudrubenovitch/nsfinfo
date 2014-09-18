78543fa794e41eccd1c12b0972d0b59ab89a4a09
Gabriel Branchaud <gabriel.branchaud@gmail.com>
2014-09-17 23:55:25 -0400

    split the nsfinfo.go & nsf.go tests in 2 files (but still in the same package).

--- nsfinfo_test.go
+++ nsfinfo_test.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
 	"testing"
 )
 
-var validHeader = []byte{
-	0x4E, 0x45, 0x53, 0x4D, 0x1A, 0x01, 0x08, 0x01, 0x60, 0x8D, 0x03, 0xA0, 0x00, 0xA0, 0x54, 0x68,
-	0x65, 0x20, 0x4C, 0x65, 0x67, 0x65, 0x6E, 0x64, 0x20, 0x6F, 0x66, 0x20, 0x5A, 0x65, 0x6C, 0x64,
-	0x61, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4B, 0x6F,
-	0x6A, 0x69, 0x20, 0x4B, 0x6F, 0x6E, 0x64, 0x6F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x31, 0x39,
-	0x38, 0x37, 0x20, 0x4E, 0x69, 0x6E, 0x74, 0x65, 0x6E, 0x64, 0x6F, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1A, 0x41,
-	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+// tests here are aimed against the methods in nsfinfo.go
 
 func TestGetHeaderWithFileNotFound(t *testing.T) {
 	h, e := getHeader("path-to-no-file")
@@ -32,62 +23,14 @@ func TestGetHeaderWithFileTooShort(t *testing.T) {
 	mustContain(t, e.Error(), "unexpected EOF")
 }
 
-func TestGetHeaderWithInvalidNsfPrelude(t *testing.T) {
-	var invalidPrelude = append([]byte(nil), validHeader...)
-	invalidPrelude[1] = 0x55 // scraps the 2nd prelude byte
-
-	h, e := getHeaderFromSlice(invalidPrelude)
+func TestGetHeaderWithInvalidHeader(t *testing.T) {
+	h, e := getHeaderFromSlice([]byte("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"))
 	hasErrorNoHeader(t, h, e)
 
-	mustContain(t, e.Error(), "invalid prelude")
+	mustContain(t, e.Error(), "invalid FourCC")
 }
 
-func TestGetHeaderWithInvalidExpansionChips(t *testing.T) {
-	var invalidExpansion = append([]byte(nil), validHeader...)
-	invalidExpansion[123] = 1 << 7 // this is one of the "future chip" bits that must never be set
-
-	h, e := getHeaderFromSlice(invalidExpansion)
-	hasErrorNoHeader(t, h, e)
-
-	mustContain(t, e.Error(), "extra sound chip section")
-}
-
-func TestGetHeaderWithValidFile(t *testing.T) {
-	h, e := getHeaderFromSlice(validHeader)
-	hasHeaderNoError(t, h, e)
-}
-
-func TestStringWithValidPALFile(t *testing.T) {
-	var palHeader = append([]byte(nil), validHeader...)
-	palHeader[122] = 1 << 0
-
-	h, e := getHeaderFromSlice(palHeader)
-	hasHeaderNoError(t, h, e)
-
-	mustContain(t, fmt.Sprint(h), ": PAL")
-}
-
-func TestStringWithValidDualRegionFile(t *testing.T) {
-	var dualRegionHeader = append([]byte(nil), validHeader...)
-	dualRegionHeader[122] = 1 << 1
-
-	h, e := getHeaderFromSlice(dualRegionHeader)
-	hasHeaderNoError(t, h, e)
-
-	mustContain(t, fmt.Sprint(h), ": dual PAL/NTSC")
-}
-
-func TestStringWithMultiChipsFile(t *testing.T) {
-	var multiChipsHeader = append([]byte(nil), validHeader...)
-	multiChipsHeader[123] = 1<<1 | 1<<3 // VRC7 & MMC5
-
-	h, e := getHeaderFromSlice(multiChipsHeader)
-	hasHeaderNoError(t, h, e)
-
-	mustContain(t, fmt.Sprint(h), ": VRC7, MMC5")
-}
-
-// the main() test is really just there to add a bit of code coverage
+// testing main() is really about adding code coverage
 func TestMainWithValidFile(t *testing.T) {
 	oldStdout := os.Stdout
 	os.Stdout = nil
@@ -97,7 +40,7 @@ func TestMainWithValidFile(t *testing.T) {
 	main()
 }
 
-// helper method that writes a test slice to disk & then calls getHeader() on it
+// getHeaderFromSlice calls getHeader() with the path to a temp file created from the byte slice
 func getHeaderFromSlice(content []byte) (*nsfHeader, error) {
 	ioutil.WriteFile("t.nsf", content, 0644)
 	defer os.Remove("t.nsf")
